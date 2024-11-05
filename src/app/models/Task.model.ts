284661c5689e3f06a248a56b6d169d607aff861c
MarioImpicciche92 <mario.impicciche@gmail.com>
2024-11-05 08:49:27 +0100

    creation injectionectionToken and add option dinamic creation

--- src/app/models/Task.model.ts
+++ src/app/models/Task.model.ts
@@ -1,3 +1,5 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
 export interface TaskModel{
@@ -5,4 +7,35 @@ export interface TaskModel{
     title:string;
     description:string;
     status:TaskStatus;
+}
+
+type TaskStatusOptions = {
+    value: 'open' | 'in-progress' | 'done',
+    taskStatus: TaskStatus,
+    text:string
+}[];
+ 
+    export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>('task-status-options');
+
+    export const  TaskStatusOptions:TaskStatusOptions = [
+    {
+        value:'open',
+        taskStatus:'OPEN',
+        text:'Open'
+    },
+    {
+        value:'in-progress',
+        taskStatus:'IN_PROGRESS',
+        text:'In_Progress'
+    },
+    {
+        value:'done',
+        taskStatus:'DONE',
+        text:'Completed'
+    }
+];
+
+export const taskStatusOptionsProvider : Provider = {
+    provide : TASK_STATUS_OPTIONS,
+    useValue : TaskStatusOptions
 }
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,16 +2,19 @@ import { Component, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskService } from '../../services/Task.service';
-
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../../models/Task.model';
 @Component({
   selector: 'app-tasks-list',
   standalone: true,
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers:[taskStatusOptionsProvider]
+  
 })
 export class TasksListComponent {
   selectedFilter = signal<string>('all');
+  taskStatusOptions = inject (TASK_STATUS_OPTIONS);
   private taskService = inject(TaskService);
   tasks =this.taskService.allTasks ;
 
